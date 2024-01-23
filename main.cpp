d1d78cf1b03d4e38280c353beed4a30fc91b1c75
Anshul Dalal <anshulusr@gmail.com>
2024-01-23 16:59:03 +0530

    dependency: added KTextEditor, KSyntaxHighlight

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
+#include <KTextEditor/Document>
+#include <KTextEditor/Editor>
+#include <KTextEditor/View>
+
 #include <QApplication>
 #include <QWidget>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    QWidget w;
-    w.show();
-    return a.exec();
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv);
+
+  QWidget main_window;
+
+  // get access to the global editor singleton
+  auto editor = KTextEditor::Editor::instance();
+
+  // create a new document
+  auto doc = editor->createDocument(&main_window);
+
+  // create a widget to display the document
+  auto view = doc->createView(&main_window);
+
+  main_window.show();
+  return a.exec();
 }
